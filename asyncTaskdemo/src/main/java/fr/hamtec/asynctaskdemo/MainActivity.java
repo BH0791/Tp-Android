58ab27ec50add9579d1e27710cc8bce2c705c6dc
HamidB <ejecteur@yahoo.fr>
2023-09-12 11:14:53 +0200

    Update -> asyncTaskDemo

--- asyncTaskdemo/src/main/java/fr/hamtec/asynctaskdemo/MainActivity.java
+++ asyncTaskdemo/src/main/java/fr/hamtec/asynctaskdemo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private EditText time;
     private TextView finalResult;
+    private static String TAG = "HB";
+    
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
@@ -21,14 +24,13 @@ public class MainActivity extends AppCompatActivity {
         time = findViewById(R.id.in_time);
         button = findViewById(R.id.btn_run);
         finalResult = findViewById(R.id.tv_result);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AsyncTaskRunner runner = new AsyncTaskRunner();
-                String sleepTime = time.getText().toString();
-                runner.execute(sleepTime);
-            }
-        });
+        button.setOnClickListener( v -> {
+            AsyncTaskRunner runner = new AsyncTaskRunner();
+            String sleepTime = time.getText().toString();
+            String toto = "tête de con";
+            runner.execute(sleepTime, toto);
+            Log.i( TAG, "onCreate: " + sleepTime );
+        } );
     }
     
     private class AsyncTaskRunner extends AsyncTask <String, String, String> {
@@ -38,15 +40,13 @@ public class MainActivity extends AppCompatActivity {
         
         @Override
         protected String doInBackground(String... params) {
-            publishProgress("Sleeping..."); // Calls onProgressUpdate()
+            Log.i( TAG, "doInBackground: " + params[0] + " => " + params[1] );
+            publishProgress("Sleeping...", "COUCOU"); // Calls onProgressUpdate()
             try {
                 int time = Integer.parseInt(params[0])*1000;
                 
                 Thread.sleep(time);
                 resp = "Slept for " + params[0] + " seconds";
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                resp = e.getMessage();
             } catch (Exception e) {
                 e.printStackTrace();
                 resp = e.getMessage();
@@ -65,16 +65,14 @@ public class MainActivity extends AppCompatActivity {
         
         @Override
         protected void onPreExecute() {
-            progressDialog = ProgressDialog.show(MainActivity.this,
-                    "ProgressDialog",
-                    "Wait for "+time.getText().toString()+ " seconds");
+            progressDialog = ProgressDialog.show(MainActivity.this,"ProgressDialog","Wait for "+time.getText().toString()+ " seconds");
         }
         
         
         @Override
         protected void onProgressUpdate(String... text) {
             finalResult.setText(text[0]);
-            
+            Log.i( TAG, "onProgressUpdate: " + text[0] + " => text[1] = " + text[1]);
         }
     }
 }
